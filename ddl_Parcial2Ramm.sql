a8dd2d1cd808105e167bb36d78d62f359cbce9bd
RafaleMarcaManrique <raphaelmarca@gmail.com>
2023-06-05 11:27:11 -0400

    Creacion de la Solucion Parcial2Ramm y la Tabla de Serie

---
+++ ddl_Parcial2Ramm.sql
@@ -0,0 +1,42 @@
+﻿-- DDL
+CREATE DATABASE Parcial2Ramm
+USE master
+GO
+
+CREATE LOGIN [usrparcial2] WITH PASSWORD = N'12345678',
+	DEFAULT_DATABASE = [Parcial2Ramm],
+	CHECK_EXPIRATION = OFF,
+	CHECK_POLICY = ON
+GO
+
+USE [Parcial2Ramm]
+GO
+
+CREATE USER [usrparcial2] FOR LOGIN [usrparcial2]
+GO
+
+ALTER ROLE [db_owner] ADD MEMBER [usrparcial2]
+GO
+
+drop table Serie;
+
+CREATE TABLE Serie(
+	id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	titulo VARCHAR(250) NOT NULL,
+	sinopsis VARCHAR(5000) NOT NULL,
+	director VARCHAR(100) NOT NULL,
+	duracion INT NOT NULL,
+	fechaEstreno DATE NOT NULL,
+	registroActivo BIT NULL DEFAULT 1,
+);
+
+-- DML
+
+CREATE PROC paSerieListar @parametro VARCHAR(250)
+AS
+  SELECT id, titulo, sinopsis, director, duracion
+  FROM Serie
+  WHERE registroActivo=1 AND titulo+director LIKE '%'+@parametro+'%'
+
+EXEC paSerieListar ''
+GO
